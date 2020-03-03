40a640f8820b8a194450ecbf41caf916c1e1fc16
ChocolateMountain <lc.letao@gmail.com>
2020-03-03 15:47:43 -0500

    Four Cell Grid solutions

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 
+### Practice Web
+
 Interview Prep - Simple HTML / CSS / Javascript Exercises
 
 Sample questions from online or made up in my head, alternative solutions may not be included
+
+---
+
+#### Environment Setup
+
+`git clone https://github.com/ChocolateMountain/practice-web.git`
+`npm install`
+`npm start`
+Launch http://localhost:3000
--- src/components/FourCellGrid.js
+++ src/components/FourCellGrid.js
@@ -1,15 +1,59 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import Description from './Description';
 
 import '../stylesheets/FourCellGrid.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export class FourCellGrid extends Component {
   render() {
+
+    const descProps = {
+      title: "Four Cell Grid",
+      description:  "Use Bootstrap Grids to produce responsive layout \n"
+                  + "Similar to Four Cell Flex"
+    };
+
     return (
       <div className="FourCellGrid">
         
+        <Description descProps={ descProps } />
 
-
-        
+        <Container>
+          <Row>
+            <Col xs={6} md={3}>
+              <div className="cell">
+                <div>
+                  Bootstrap Grid Settings
+                </div>
+                <div>
+                  xs=6, md=3
+                </div>
+              </div>
+            </Col>
+            <Col xs={6} md={3}>
+              <div className="cell">
+                <div>
+                  xs=6, md=3
+                </div>
+              </div>
+            </Col>
+            <Col xs={6} md={3}>
+              <div className="cell">
+                <div>
+                  xs=6, md=3
+                </div>
+              </div>
+            </Col>
+            <Col xs={6} md={3}>
+              <div className="cell">
+                <div>
+                  xs=6, md=3
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
       </div>
     )
   }
